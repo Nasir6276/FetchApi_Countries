6628bdd9d16aea3dd19979a210707aa8f92db2ed
Nasir6276 <ni3262019@gmail.com>
2023-08-18 19:44:51 +0100

    third commit

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const axios = require('axios')
+const dotenv = require('dotenv').config()
 
 const options = {
     method: 'GET',
     url: 'https://rest-country-api.p.rapidapi.com/',
     headers: {
-    'X-RapidAPI-Key': '174ce9e783msh79d4287c19b00d7p13f2ddjsn126339845e10',
+    'X-RapidAPI-Key': process.env.APIKEY,
     'X-RapidAPI-Host': 'rest-country-api.p.rapidapi.com'
     }
   };
@@ -12,7 +13,11 @@ const options = {
 async function getCountries() {
     try {
         const response = await axios.request(options);
-        console.log(response.data);
+        const countries = response.data
+        countries.forEach(name => {
+            console.log(name);
+        });
+        console.log(countries);
     } catch (error) {
         console.error(error);
     }
